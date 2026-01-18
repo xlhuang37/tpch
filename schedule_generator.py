8db4dc71ff2d33b0b956fde5f1372bfefd068de0
Xiaolong Huang <xlhuang2002@gmail.com>
2026-01-17 21:38:43 -0500

    sched

--- schedule_generator.py
+++ schedule_generator.py
@@ -39,8 +39,9 @@ SCHEDULES: List[ScheduleConfig] = [
     ScheduleConfig(load=16, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
     ScheduleConfig(load=16, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
     ScheduleConfig(load=20, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
-
     ScheduleConfig(load=20, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
+    ScheduleConfig(load=24, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
+    ScheduleConfig(load=24, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
     ]
 
 # Directory containing query SQL files

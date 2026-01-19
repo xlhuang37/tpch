2ff95d26c0c41cbc8298a39755960c7a8c7f5e4c
Xiaolong Huang <xlhuang2002@gmail.com>
2026-01-18 22:24:37 -0500

    .

--- schedule_generator.py
+++ schedule_generator.py
@@ -34,14 +34,16 @@ class ScheduleConfig:
 # Define all schedules to generate
 SCHEDULES: List[ScheduleConfig] = [
     # # Load Config
-    ScheduleConfig(load=12, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
-    # ScheduleConfig(load=12, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
-    # ScheduleConfig(load=16, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
-    # ScheduleConfig(load=16, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
-    # ScheduleConfig(load=20, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
-    # ScheduleConfig(load=20, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
-    # ScheduleConfig(load=24, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
-    ScheduleConfig(load=24, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
+    #ScheduleConfig(load=12, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
+    #ScheduleConfig(load=12, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
+    #ScheduleConfig(load=16, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
+    #ScheduleConfig(load=16, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
+    ScheduleConfig(load=28, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
+    ScheduleConfig(load=28, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
+    ScheduleConfig(load=32, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
+    ScheduleConfig(load=32, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
+    ScheduleConfig(load=36, ratio=1.0, length=3600, query_type="greedy", p_size=140, np_size=58),
+    ScheduleConfig(load=36, ratio=1.0, length=3600, query_type="default", p_size=140, np_size=58),
     ]
 
 # Directory containing query SQL files

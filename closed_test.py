e23b2045d9a0dc4352019b4ae6169bc30a9980b5
Xiaolong Huang <xlhuang2002@gmail.com>
2026-01-22 15:02:59 -0500

    required

--- closed_test.py
+++ closed_test.py
@@ -702,7 +702,7 @@ def main():
     )
     ap.add_argument(
         "--queries-dir", 
-        default="./queries/",
+        required=True,
         help="Directory containing SQL files to benchmark (default: ./queries/)"
     )
     ap.add_argument(

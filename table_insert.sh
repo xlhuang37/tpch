db1d7737546c4c83564cf1cd413e2f88e77bb694
Xiaolong Huang <xlhuang2002@gmail.com>
2026-01-25 10:58:00 -0500

    data

--- table_insert.sh
+++ table_insert.sh
@@ -1,7 +1,35 @@
+echo "deleting existing tables"
+rm -rf ../runtime
+
+if [ "$1" == "-s" ]; then
+    # 1. Check if $2 is empty
+    if [ -z "$2" ]; then
+        echo "Error: -s requires a numeric value."
+        exit 1
+    fi
+
+    # 2. Check if $2 is a number (using a Regular Expression)
+    if [[ ! "$2" =~ ^[0-9]+$ ]]; then
+        echo "Error: '$2' is not a valid number."
+        exit 1
+    fi
+
+    size=$2
+    echo "Setting size to $size"
+else 
+    echo "Error: Invalid argument. Use -s <size> to specify the size of the database."
+    exit 1
+fi
+
+
 git submodule update --init --recursive
 
 ../ClickHouse/build/programs/clickhouse-format -n --oneline < ./templates/table_create.sql > ./templates/table_create_processed.sql
 ../ClickHouse/build/programs/clickhouse-client < ./templates/table_create_processed.sql
+
+cd ./tpch-kit/dbgen
+make
+./dbgen -s 
 
 ../ClickHouse/build/programs/clickhouse-client --format_csv_delimiter '|' --query "INSERT INTO nation FORMAT CSV" < ./tpch-kit/dbgen/nation.tbl
 ../ClickHouse/build/programs/clickhouse-client --format_csv_delimiter '|' --query "INSERT INTO region FORMAT CSV" < ./tpch-kit/dbgen/region.tbl

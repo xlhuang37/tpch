16d49c4f8e8a4fe35016bf3941445c76b37092b1
Xiaolong Huang <xlhuang2002@gmail.com>
2026-01-25 15:35:16 -0500

    diff query

--- queries/021_tpch_default.sql
+++ queries/021_tpch_default.sql
@@ -1,39 +1,43 @@
 SELECT
+    s_acctbal,
     s_name,
-    count(*) AS numwait
+    n_name,
+    p_partkey,
+    p_mfgr,
+    s_address,
+    s_phone,
+    s_comment
 FROM
+    part,
     supplier,
-    lineitem l1,
-    orders,
-    nation
+    partsupp,
+    nation,
+    region
 WHERE
-    s_suppkey = l1.l_suppkey
-    AND o_orderkey = l1.l_orderkey
-    AND o_orderstatus = 'F'
-    AND l1.l_receiptdate > l1.l_commitdate
-    AND EXISTS (
-        SELECT
-            *
-        FROM
-            lineitem l2
-        WHERE
-            l2.l_orderkey = l1.l_orderkey
-            AND l2.l_suppkey <> l1.l_suppkey
-    )
-    AND NOT EXISTS (
-        SELECT
-            *
-        FROM
-            lineitem l3
-        WHERE
-            l3.l_orderkey = l1.l_orderkey
-            AND l3.l_suppkey <> l1.l_suppkey
-            AND l3.l_receiptdate > l3.l_commitdate
-    )
+    p_partkey = ps_partkey
+    AND s_suppkey = ps_suppkey
+    AND p_size = 15
+    AND p_type LIKE '%BRASS'
     AND s_nationkey = n_nationkey
-    AND n_name = 'SAUDI ARABIA'
-GROUP BY
-    s_name
+    AND n_regionkey = r_regionkey
+    AND r_name = 'EUROPE'
+    AND ps_supplycost = (
+        SELECT
+            min(ps_supplycost)
+        FROM
+            partsupp,
+            supplier,
+            nation,
+            region
+        WHERE
+            p_partkey = ps_partkey
+            AND s_suppkey = ps_suppkey
+            AND s_nationkey = n_nationkey
+            AND n_regionkey = r_regionkey
+            AND r_name = 'EUROPE'
+    )
 ORDER BY
-    numwait DESC,
-    s_name;
+    s_acctbal DESC,
+    n_name,
+    s_name,
+    p_partkey;

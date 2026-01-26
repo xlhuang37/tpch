15cd2079d5b5adead2d61185149e358fc0e0dc40
Xiaolong Huang <xlhuang2002@gmail.com>
2026-01-25 22:04:49 -0500

    ..

--- queries/extreme_aws/greedy/np_greedy_58.sql
+++ queries/extreme_aws/greedy/np_greedy_58.sql
@@ -25,4 +25,4 @@ ORDER BY
     l_returnflag,
     l_linestatus
 SETTINGS
-    workload='SpeedUpOne';
+    workload='SpeedUpTwo';
--- queries/np_all_58.sql
+++ queries/np_all_58.sql
@@ -25,4 +25,4 @@ ORDER BY
     l_returnflag,
     l_linestatus
 SETTINGS
-    workload='SpeedUpTwo';
+    workload='SpeedUpOne';
